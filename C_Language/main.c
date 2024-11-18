f4d4da69c35e4fe02605fef6f57b909c8880c6d8
Joao Pedro Rodrigues <jp.rgs.2015@gmail.com>
2024-11-18 20:25:03 -0300

    aloc_memory

---
+++ C_Language/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    
+    //comentario apenas na linha
+
+    /* 
+    comentario
+    longo
+    */ 
+
+   int idade;
+   char nome; // 'string' 
+   // float 
+   // double  
+
+   idade = 23;
+   nome = joao;
+
+    printf("Minha idade e' %d e meu nome e' %c\n", idade,nome); // formato para variáveis
+
+    return 0;
+}   
